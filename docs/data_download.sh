08069d6728e9e9f68735293f9da15a0b0d7edb80
Xander Bystrom <bystromxander@gmail.com>
2026-01-29 16:30:31 -0700

    Removed docs/data directory and restructured data download bash script to not use docs/data

--- docs/data_download.sh
+++ docs/data_download.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 
-# remove the opinf directory if it already exists
-if [ -d "opinf" ]; then
-    echo "Removing existing opinf/ directory..."
-    rm -rf opinf/
+# NOTE: this script should be run from the docs/ directory
+
+# remove the temp_data directory if it already exists
+if [ -d "temp_data" ]; then
+    echo "Removing existing temp_data/ directory..."
+    rm -rf temp_data/
 fi
 
 # Clone into the repository and download only the data branch
 echo "Cloning into the data branch of https://github.com/operator-inference/opinf.git..."
-git clone -b data --single-branch --depth=1 https://github.com/operator-inference/opinf.git
+git clone -b data --single-branch --depth=1 https://github.com/operator-inference/opinf.git temp_data/
 
 # move docs files into api/ directory and all other data files into the data/ directory
 echo "Moving basis_example.npy, pre_example.npy, and lstsq_example.npz into docs/source/api/ directory..."
-mv opinf/basis_example.npy opinf/lstsq_example.npz opinf/pre_example.npy source/api/
+mv temp_data/basis_example.npy source/api/
+mv temp_data/lstsq_example.npz source/api/
+mv temp_data/pre_example.npy source/api/
 
-echo "Moving all other data files into data/ directory..."
-mv opinf/* data/ 
+echo "Moving basics_data.h5, inputs_data.h5, and parametric_data.h5 into docs/source/tutorials/ directory..."
+mv temp_data/basics_data.h5 source/tutorials/
+mv temp_data/inputs_data.h5 source/tutorials/
+mv temp_data/parametric_data.h5 source/tutorials/
 
-echo "Removing empty opinf/ directory..."
-rm -rf opinf/
+echo "Removing empty temp_data/ directory..."
+rm -rf temp_data/
 
 echo "Done!"
---
+++ docs/data_gen_script.py
@@ -0,0 +1,132 @@
+from collections.abc import Callable
+import pathlib
+import sys
+
+import numpy as np
+import scipy
+import opinf
+
+
+def generate_training_data(
+    n_samples: int,
+    n_timesteps: int,
+    q_0: Callable[[np.ndarray], np.ndarray],
+    u: Callable[[int], np.ndarray] | None = None,
+):
+    """Generate sample data to be used for Operator Inference.
+
+    Args:
+    n_samples: Number of spatial samples.
+    n_timesteps: Number of time steps.
+    q_0: Initial condition function. Accepts an array of spatial locations and
+        returns an array of initial condition values.
+    u: External input function. Accepts a time value and returns the input
+        values for that time step. If None, u defaults is the zero function
+        (for a model that does not use external inputs)
+
+    Returns:
+    t: Array of time points.
+    Q: Array of "observed" snapshots, shape (n_samples, n_timesteps).
+    """
+    external_inputs = True
+    if u is None:
+        external_inputs = False
+
+        def u(t):
+            return 0
+
+    # construct the spatial and temporal domains
+    x = np.linspace(0, 1, n_samples + 2)[1:-1]
+    dx = x[1] - x[0]
+    t = np.linspace(0, 1, n_timesteps)
+
+    # construct the matrix of linear operators
+    diags = np.array([1, -2, 1]) / dx**2
+    A = scipy.sparse.diags(diags, [-1, 0, 1], (n_samples, n_samples))
+
+    # construct the matrix of external input operators
+    B = np.zeros_like(x)
+    B[0], B[-1] = 1 / dx**2, 1 / dx**2
+
+    fom = opinf.models.ContinuousModel(
+        operators=[
+            opinf.operators.LinearOperator(A),
+            opinf.operators.InputOperator(B),
+        ]
+    )
+
+    initial_values = q_0(x)
+    initial_values = (
+        initial_values if not external_inputs else initial_values * u(0)
+    )
+
+    return t, fom.predict(initial_values, t, input_func=u, method="BDF")
+
+
+def save_data_to_file(
+    t: np.ndarray, Q: np.ndarray, filepath: str, overwrite=True
+):
+    with opinf.utils.hdf5_savehandle(filepath, overwrite=overwrite) as h5file:
+        h5file.create_dataset("t", data=t)
+        h5file.create_dataset("Q", data=Q)
+
+    print(f"Training data saved to {filepath}")
+
+
+def generate_basics_data(filepath: str = "basics_data.h5"):
+    n_samples = 512
+    n_timesteps = 401
+
+    def q_0(x):
+        return x * (1 - x)
+
+    t, Q = generate_training_data(n_samples, n_timesteps, q_0)
+
+    save_data_to_file(t, Q, str(path), overwrite=True)
+
+
+def generate_external_inputs_data(filepath: str = "inputs_data.h5"):
+    n_samples = 512
+    n_timesteps = 1000
+
+    alpha = 100
+
+    # the part of the initial condition independent of u(t)
+    def q_0(x):
+        return np.exp(alpha * (x - 1)) + np.exp(-alpha * x) - np.exp(-alpha)
+
+    # the external input function
+    def u(t):
+        return np.ones_like(t) + np.sin(4 * np.pi * t) / 4
+
+    t, Q = generate_training_data(n_samples, n_timesteps, q_0, u)
+
+    save_data_to_file(t, Q, filepath, overwrite=True)
+
+
+if __name__ == "__main__":
+    BASE_DIR = pathlib.Path(__file__).resolve().parent
+    data_to_generate = None
+
+    if len(sys.argv) < 2:
+        data_to_generate = "basics"
+    else:
+        if sys.argv[1] in ["basics", "inputs", "parametric"]:
+            data_to_generate = sys.argv[1]
+        else:
+            raise ValueError(
+                "Data to generate must be one of the following: "
+                "'basics', 'inputs', or 'parametric'."
+            )
+
+    filename = data_to_generate + "_data.h5"
+    path = str(BASE_DIR / "source" / "tutorials" / filename)
+
+    if data_to_generate == "basics":
+        generate_basics_data(path)
+    elif data_to_generate == "inputs":
+        generate_external_inputs_data(path)
+    elif data_to_generate == "parametric":
+        raise NotImplementedError(
+            "Parametric data generation has not yet been implemented!"
+        )
