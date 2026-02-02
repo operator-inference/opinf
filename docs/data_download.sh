541d029e49949e5d469be2c6276adc5265ca6d6a
Xander Bystrom <bystromxander@gmail.com>
2026-02-02 11:56:05 -0700

    Updated data download bash script with better directory organization

--- docs/data_download.sh
+++ docs/data_download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# NOTE: this script should be run from the docs/ directory
+# NOTE: this script should be run from the root of the opinf directory
 
 # remove the temp_data directory if it already exists
 if [ -d "temp_data" ]; then
@@ -9,19 +9,20 @@ if [ -d "temp_data" ]; then
 fi
 
 # Clone into the repository and download only the data branch
-echo "Cloning into the data branch of https://github.com/operator-inference/opinf.git..."
-git clone -b data --single-branch --depth=1 https://github.com/operator-inference/opinf.git temp_data/
+echo "Cloning into the data branch of https://github.com/XanderBys/opinf.git..."
+git clone -b data --single-branch --depth=1 https://github.com/XanderBys/opinf.git temp_data/
 
-# move docs files into api/ directory and all other data files into the data/ directory
+# move docs files into docs/source/api/ directory
 echo "Moving basis_example.npy, pre_example.npy, and lstsq_example.npz into docs/source/api/ directory..."
-mv temp_data/basis_example.npy source/api/
-mv temp_data/lstsq_example.npz source/api/
-mv temp_data/pre_example.npy source/api/
+mv temp_data/basis_example.npy docs/source/api/
+mv temp_data/lstsq_example.npz docs/source/api/
+mv temp_data/pre_example.npy docs/source/api/
 
+# move tutorial data files into docs/source/tutorials/ directory
 echo "Moving basics_data.h5, inputs_data.h5, and parametric_data.h5 into docs/source/tutorials/ directory..."
-mv temp_data/basics_data.h5 source/tutorials/
-mv temp_data/inputs_data.h5 source/tutorials/
-mv temp_data/parametric_data.h5 source/tutorials/
+mv temp_data/basics_data.h5 docs/source/tutorials/
+mv temp_data/inputs_data.h5 docs/source/tutorials/
+mv temp_data/parametric_data.h5 docs/source/tutorials/
 
 echo "Removing empty temp_data/ directory..."
 rm -rf temp_data/
